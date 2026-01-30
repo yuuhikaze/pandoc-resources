464c793e3d974db2d6ac7c2ce85149b522ef4c64
yuuhikaze <yuuhikaze@protonmail.com>
2026-01-29 21:38:44 -0500

    feat: adds flake.nix and docs devshell

---
+++ devshells/docs.nix
@@ -0,0 +1,34 @@
+{ pkgs, tern-core, resources-root }:
+
+pkgs.mkShell {
+  name = "tern+pandoc_resources-shell";
+
+  buildInputs = [ pkgs.pandoc tern-core ];
+
+  shellHook = ''
+    # Set up Tern converter
+    mkdir -p ~/.local/share/tern/converters
+    ln -sf ${resources-root}/tern-converters/pandoc.lua ~/.local/share/tern/converters/pandoc.lua
+
+    # Locate docs dir (build)
+    DOCS_DIR=$(dirname "$(readlink -f "$0")")
+    DOCS_DIR="$\{DOCS_DIR%/docs*}/docs"
+    echo $DOCS_DIR
+
+    # Symlink pandoc-templates into docs dir
+    mkdir "$DOCS_DIR/pandoc-templates"
+    ln -sf ${resources-root}/pandoc-templates/docs.html "$DOCS_DIR/pandoc-templates"
+    ln -sf ${resources-root}/pandoc-templates/docs.css "$DOCS_DIR/pandoc-templates"
+
+    # Export convenience environment variables
+    export PANDOC_DOCS_HTML="$DOCS_DIR/pandoc-templates/docs.html"
+    export PANDOC_DOCS_CSS="$DOCS_DIR/pandoc-templates/docs.css"
+
+    # Print information
+    echo "====== Documentation Environment ======"
+    echo "Tern:   $(tern-core --version)"
+    echo "Pandoc: $(pandoc --version | head -n 1)"
+    echo "Resources synced ✓"
+    echo "======================================="
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    tern-src.url = "github:yuuhikaze/tern";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, tern-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tern-core = tern-src.packages.${system}.tern-core;
+      in {
+        # Export docs devshell for external use
+        devShells.tern-pandoc = import ./devshells/docs.nix {
+          inherit pkgs tern-core;
+          resources-root = self;
+        };
+      });
+}
